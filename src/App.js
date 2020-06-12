0c48baccfdb9b296fb357e8b6b959c4abad89877
3Bartnik <ebartnik@o2.pl>
2020-06-12 15:24:25 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Nav from "./Nav";
+import Error from "./Error";
+import Footer from "./Footer";
+import Studio from "./Studio";
+import Price from "./Price";
+import Artistic from "./Artistic";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <section style={{ margin: "0 0 50px 0" }}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/studio" component={Studio} />
+            <Route path="/price" component={Price} />
+            <Route path="/artistic" component={Artistic} />
+            <Route component={Error} />
+          </Switch>
+        </section>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
